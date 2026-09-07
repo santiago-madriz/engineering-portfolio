0f94f0b62f3b461efdecefed88ab40de8ddcd7c7
santiago-madriz <santiagomadrizc@gmail.com>
2026-09-07 01:04:57 -0600

    Support GitHub Pages preview path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://santiago-madriz.dev',
+  base: process.env.GITHUB_PAGES === 'true' ? '/santiago-madriz.dev' : '/',
   output: 'static'
 });
